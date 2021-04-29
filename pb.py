8722f5e72750399b032af40ff05dae8c17b8899b
tsunadeka <78367794+Lina2812@users.noreply.github.com>
2021-04-29 13:09:37 +0300

    Add files via upload

---
+++ pb.py
@@ -0,0 +1,31 @@
+i = 0
+a, b = map(int, input().split()) 
+a1 = 0
+b1 = 0
+st = 1
+r = 0
+while i == 0:
+    comand = input()
+    if comand == "вперед":
+        a1 += st
+    elif comand == "направо":
+        a1, b1 = b1, a1
+        if st == -1:
+            st == -st
+        r += 1
+    elif comand == "налево":
+        a1, b1 = b1, a1
+        if st ==  1:
+            st = -st
+        r -= 1
+    elif comand == "разворот":
+        st = -1
+    elif comand == "стоп":
+        if r % 2 == 0:
+            i += 1
+            print("вы нашли клад,он находится на",a1,b1)
+        else:
+            i += 1
+            print("вы нашли клад,он находится на",b1,a1)
+    else:
+        print("такой команды нет")
